ad6a826104269ba2232502c6990036614812234d
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-02-22 20:40:23 +0300

    Add urls for submenu

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from menu.views import index
+from menu.views import index, draw_menu
 
 urlpatterns = [
     path('', index, name='headers'),
+    path('<path:path>/', draw_menu, name='draw_menu'),
 ]
