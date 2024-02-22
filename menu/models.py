117bba1fe9b8a1a8bf88689f96286e8823557746
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-02-22 13:01:44 +0300

    Add header model

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MenuHeader(models.Model):
+    title = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.title
