f06ece283590469e687d43a6396d87e7fc54428b
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-02-22 16:04:48 +0300

    Add view for main template

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from menu.models import MenuHeader
+
+
+def index(request):
+    return render(request, 'menu/index.html', context={'headers': MenuHeader.objects.all()})
