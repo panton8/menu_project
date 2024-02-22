0bb9d088244ab4f2a6b493eb033a4f9a235f72fb
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-02-22 18:41:36 +0300

    Add view for menu drawing

--- menu/views.py
+++ menu/views.py
@@ -6,3 +6,9 @@ from menu.models import MenuHeader
 
 def index(request):
     return render(request, 'menu/index.html', context={'headers': MenuHeader.objects.all()})
+
+
+def draw_menu(request, path):
+    splitted_path = path.split('/')
+    return render(
+        request, 'menu/index.html', context={'header': splitted_path[0], 'item': splitted_path[-1]})
