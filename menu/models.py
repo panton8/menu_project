aa798032e977f224810a5ce813652b77fc00f263
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-02-22 13:13:35 +0300

    Add menu's item model

--- menu/models.py
+++ menu/models.py
@@ -8,3 +8,13 @@ class MenuHeader(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class MenuItem(models.Model):
+    header = models.ForeignKey(MenuHeader, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60, unique=True)
+    url = models.URLField(blank=True)
+    root = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
